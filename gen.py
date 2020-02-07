a14ce99008bf41e3a8b721782be17610ae613d0f
Kyle Tolentino <kylett2@illinois.edu>
2020-02-07 13:29:04 -0600

    Made generator random w/ 1s and 0s

--- gen.py
+++ gen.py
@@ -1,6 +1,13 @@
 import numpy as np
 import pickle
 
-empty_grid = np.zeros((3,3))
-with open('empty.pkl', 'wb') as outfile:
-    pickle.dump(empty_grid, outfile)
+# empty_grid = np.zeros((3,3))
+# with open('empty.pkl', 'wb') as outfile:
+#    pickle.dump(empty_grid, outfile)
+
+
+d = 3;
+
+test = np.random.randint(2, size=(d,d))
+with open('test.pkl', 'wb') as outfile:
+   pickle.dump(test, outfile)
