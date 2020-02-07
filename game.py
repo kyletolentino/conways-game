6bf8072a6e4869513203dcf81fbc7a8ace05c8c1
Kyle Tolentino <kylett2@illinois.edu>
2020-02-07 14:16:00 -0600

    Defined corners, sides, and else in loops

--- game.py
+++ game.py
@@ -1,16 +1,22 @@
 import numpy as np
 import pickle
 
-# ex = np.array([[0, 1, 3], [4, 5, 6], [7, 8, 9]])
-ex = pickle.load(open("test.pkl", 'rb'))
+# calling my functions
+# import functions.py as func
+
+# ex = pickle.load(open("test.pkl", 'rb'))
+ex = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 temp = ex
 
-print("Original \n")
 print(ex)
-
 
 for x in range(ex.shape[0]):
     for y in range(ex.shape[1]):
+
         # corner case
         if ((x==0 or x==ex.shape[0]-1) and (y==0 or y==ex.shape[1]-1)):
-            print(temp[x][y])
+            print("Corner: ", ex[x][y])
+        elif ((x==0 or x==ex.shape[0]-1) or (y==0 or y==ex.shape[1]-1)):
+            print("Side: ", ex[x][y])
+        else:
+            print("Else: ", ex[x][y])
