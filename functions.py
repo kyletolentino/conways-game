e45356454d6726e9ca3affa648d91c905d549064
Kyle Tolentino <kylett2@illinois.edu>
2020-02-07 15:26:45 -0600

    Taking break, saving progress

--- functions.py
+++ functions.py
@@ -12,16 +12,25 @@ import numpy as np
 
 
 def check(position, state):
+    # position will be tuple (x,y)
+    # state is the current gen
     x = position[0]
     y = position[1]
     count = 0
 
     # created two for loops to get # of 1s around given position
     for i in range(x-1, x+2):
+        if (i<0 or i>state.shape[0]):
+            continue
         for j in range(y-1, y+2):
-            # needed this to prevent cell from counting itself (in case it was 1)
+            if (j<0 or j>state.shape[1]):
+                continue
+
+            # need to prevent cell from counting itself (in case it was 1)
             if(i == x and j == y):
                 continue
+
+            # count up all 1s surrounding cell
             elif (state[i][j] == 1):
                 count += 1
     
--- game.py
+++ game.py
@@ -2,25 +2,33 @@ import numpy as np
 import pickle
 
 # calling my functions
-# import functions.py as func
+import functions as func
 
-# ex = pickle.load(open("test.pkl", 'rb'))
-ex = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+ex = pickle.load(open("test.pkl", 'rb'))
+display = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+
+# need to create temporary state before looping
+# don't want to access edited values while iterating
+# i.e. something changed in row 1, but row 2 needs original value
 temp = ex
 
 print(ex)
+print("")
+
 
 for x in range(ex.shape[0]):
     for y in range(ex.shape[1]):
 
         # corner case
         if ((x==0 or x==ex.shape[0]-1) and (y==0 or y==ex.shape[1]-1)):
-            print("Corner: ", ex[x][y])
+            ex[x][y] = func.check((x,y), temp)
 
         # side case
         elif ((x==0 or x==ex.shape[0]-1) or (y==0 or y==ex.shape[1]-1)):
-            print("Side: ", ex[x][y])
+            continue
 
         # everything else
         else:
-            print("Else: ", ex[x][y])
+            ex[x][y] = func.check((x,y), temp)
+
+print(ex)
