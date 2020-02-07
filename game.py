cd35614a2066dfe41fba036f969ee7fcc56fa387
Kyle Tolentino <kylett2@illinois.edu>
2020-02-07 13:29:52 -0600

    Set up loops for corner cases

--- game.py
+++ game.py
@@ -1,10 +1,16 @@
 import numpy as np
 import pickle
 
-ex = np.array([[0, 1, 0], [0, 1, 0], [0, 1, 0]])
+# ex = np.array([[0, 1, 3], [4, 5, 6], [7, 8, 9]])
+ex = pickle.load(open("test.pkl", 'rb'))
+temp = ex
 
-for i in range(ex.shape[0]):
-    for j in range(ex.shape[1]):
-        print(ex[i][j])
+print("Original \n")
+print(ex)
 
-# test
+
+for x in range(ex.shape[0]):
+    for y in range(ex.shape[1]):
+        # corner case
+        if ((x==0 or x==ex.shape[0]-1) and (y==0 or y==ex.shape[1]-1)):
+            print(temp[x][y])
