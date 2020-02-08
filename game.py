45af54932736bd45af0283589af36a6f45374ea4
Kyle Tolentino <kylett2@illinois.edu>
2020-02-07 18:22:45 -0600

    finished argparse

--- game.py
+++ game.py
@@ -8,13 +8,17 @@ ex = pickle.load(open("input.pkl", 'rb'))
 print("")
 print(ex)
 
-n = 4
+parser = argparse.ArgumentParser(description='How many generations?', epilog='Hello there ;)')
+parser.add_argument('gens', nargs=1, type=int, help = "Will run Conway's Game of Life for n generations")
+args = parser.parse_args()
 
+n = int(args.gens[0])
+
+# if you just want to see the end state, delete the print statments inside runs()
 def runs(state, n):
     count = n
     if count<=1:
         print("")
-        #print("    |")
         print("    V")
         print("")
         return func.step(state)
@@ -22,11 +26,16 @@ def runs(state, n):
         count -= 1
         next = func.step(state)
         print("")
-        #print("    |")
         print("    V")
         print("")
         print(next)
         return runs(next, count)
 
-print(runs(ex, n))
+output = runs(ex, n)
+print(output)
 print("")
+print("Created output.pkl")
+print("")
+
+with open('output.pkl', 'wb') as outfile:
+   pickle.dump(output, outfile)
