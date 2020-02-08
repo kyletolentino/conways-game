05addf76821b4b4abb1ca25524cd4be880465475
Kyle Tolentino <kylett2@illinois.edu>
2020-02-07 18:52:56 -0600

    added debug,py for seeing output

---
+++ debug.py
@@ -0,0 +1,9 @@
+import numpy as np
+import functions as func
+import pickle
+
+# just used this to see output.pkl from another python file
+test = pickle.load(open("output.pkl", 'rb'))
+print("")
+print(test)
+print("")
