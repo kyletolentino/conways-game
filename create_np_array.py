761b7668e352dda3f36a6c1a203b778aaec832cb
Kyle Tolentino <kyltol@gmai>
2020-02-10 10:33:50 -0600

    Updated comments

--- create_np_array.py
+++ create_np_array.py
@@ -3,25 +3,32 @@ import functions as func
 import random
 import pickle
 
+# x1 and x2 are feature vectors, i.e. first matrix and sequential matrix
+# y will be labels
 x1 = []
 x2 = []
 y = []
 
+# generate 200000 samples (the more the merrier!)
+# NOTE: I only ran this once because the it would generate random data each time it runs
 for i in range(200000):
     pick = random.randint(0, 1)
     state = np.random.randint(2, size=(3, 3))
 
-    # if pick is 0, create fake step, else make it real
+    # if pick is 0, create fake step, w/ label 0
     if (pick == 0):
         x1.append(state)
         x2.append(func.false_step(state))
         y.append(0)
+    # if pick is 1, create real step, w/ label 1
     else:
         x1.append(state)
         x2.append(func.step(state))
         y.append(1)
 
+# store each array into another list
 data = [x1, x2, y]
 
+# save as data
 with open('data.pkl', 'wb') as outfile:
     pickle.dump(data, outfile)
