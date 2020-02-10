0d576d912b7bd34b81cb0d50d691fb22643dfa36
Kyle Tolentino <kyltol@gmai>
2020-02-10 10:40:10 -0600

    Updated comments

--- debug.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import functions as func
-import pickle
-
-# just used this to see output.pkl from another python file
-test = pickle.load(open("output.pkl", 'rb'))
-print("")
-print(test)
-print("")
--- game.py
+++ game.py
@@ -4,12 +4,13 @@ import argparse
 # this is my functions.py module
 import functions as func
 
+# load input and display it
 ex = pickle.load(open("input.pkl", 'rb'))
 print("")
 print(ex)
 
 # run python gen.py n, where n = # of generations to pass
-# default is n=1 (i.e. one step)
+# default is n = 1 (i.e. one step)
 parser = argparse.ArgumentParser(description='How many generations?', epilog='Hello there ;)')
 parser.add_argument('gens', type=int, nargs='?', const=1, help = "Will run Conway's Game of Life for n generations", default=1)
 args = parser.parse_args()
@@ -22,7 +23,8 @@ output = func.runs(ex, n)
 print(output)
 print("")
 print("Created output.pkl")
-print("")
+print("")ga
 
+# save the output
 with open('output.pkl', 'wb') as outfile:
    pickle.dump(output, outfile)
