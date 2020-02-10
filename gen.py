90687558982ab9354332332b6b82a94fba89b952
Kyle Tolentino <kyltol@gmai>
2020-02-10 10:43:17 -0600

    Removed redundancies, updated comments

--- gen.py
+++ gen.py
@@ -2,9 +2,12 @@ import numpy as np
 import pickle
 import argparse
 
+# NOTE: if you want to generate n x m numpy arrays instead of m x m,
+#  edit input line to be size = (dim, h)
+
 # will let gen.py take arguments for dim
 # if only one argument, grid will be square
-# if 2 arguments, grid will be w xinput = np.random.randint(2, size=(dim,dim)) h - see *
+# if 2 arguments, grid will be w xinput = np.random.randint(2, size=(dim,dim))
 parser = argparse.ArgumentParser(description="Choose dimension of grid")
 parser.add_argument('length', const=3, nargs='?',type=int, default=3)
 parser.add_argument('height', const=3, nargs='?', type=int, default=3)
@@ -15,11 +18,12 @@ dim = args.length
 
 # can also choose a non-square shape
 # *must change (dim, dim) to (w,h) within 'input'
-w = dim
 h = args.height
 
 
-input = np.random.randint(2, size=(dim,dim))
+# create a random numpy array with values of 0s and 1s
+input = np.random.randint(2, size=(dim, dim))
+
 # print input to see if you like, if not then run gen.py again
 print(input)
 
