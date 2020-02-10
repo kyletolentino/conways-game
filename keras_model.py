11b0627a595d24b7ddacf2db0a70dfebcc9f2efc
Kyle Tolentino <kyltol@gmai>
2020-02-09 23:46:32 -0600

    Created shared layer, simplified network

--- keras_model.py
+++ keras_model.py
@@ -1,12 +1,11 @@
 import numpy as np
 import pickle
-from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import tensorflow as tf
 
 # put these first for ease when tuning
-epoch_size = 20
-batch = 512
+epoch_size = 50
+batch = 128
 
 # tried to use pandas and csv
 # but modern problems require modern solutions (right?)
@@ -16,26 +15,27 @@ x1 = data[0]
 x2 = data[1]
 y = data[2]
 
-# merge both matrices into one to identify trends - possibly CNN approach
-# will have to concatenate test data (i.e. when given two numpy arrays)
-X = np.concatenate((x1, x2), axis=1)
+x1 = np.array([m for m in x1])
+x2 = np.array([m for m in x2])
+y = np.array(y)
 
-# random_state should always be kept the same for model selection
-# I picked random_state = 7 ;)
-X_train, X_val, y_train, y_val = train_test_split(X, y, random_state=7, test_size=0.2)
+# doing a shared layer, then will concatenate dual inputs
+x_input = tf.keras.Input(shape=(3, 3))
 
+layer1 = tf.keras.layers.Dense(64, activation='relu')
+x1_layer = layer1(x_input)
+x2_layer = layer1(x_input)
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(128, input_shape=(6, 3), activation='relu'),
-    tf.keras.layers.Dropout(0.1),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dropout(0.1),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(1, activation='sigmoid')
-])
+merged_vector = tf.keras.layers.concatenate([x1_layer, x2_layer], axis=-1)
+flat_layer = tf.keras.layers.Flatten()(merged_vector)
+dense2 = tf.keras.layers.Dense(32, activation='relu')(flat_layer)
+predictions = tf.keras.layers.Dense(1, activation='sigmoid')(dense2)
 
+model = tf.keras.Model(inputs=x_input, outputs=predictions)
+
+# SGD + momentum optimizer for shallow networks
 model.compile(loss='binary_crossentropy',
-              optimizer='adam',
+              optimizer=tf.keras.optimizers.SGD(lr=0.01, nesterov=True),
               metrics=['accuracy'])
 
 model.summary()
@@ -43,19 +43,7 @@ model.summary()
 # if we see validation loss decreasing, stop training
 callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=3)
 
-history = model.fit(X_train, y_train, epochs=epoch_size, batch_size=batch,
-                    validation_data=(X_val, y_val), callbacks=[callback], shuffle=True)
+history = model.fit(x1+x2, y, epochs=epoch_size, batch_size=batch,
+                    validation_split=0.2, callbacks=[callback], shuffle=True)
 
-
-def plot_graphs(history, string):
-    plt.plot(history.history[string])
-    plt.plot(history.history['val_'+string], '')
-    plt.xlabel("Epochs")
-    plt.ylabel(string)
-    plt.legend([string, 'val_'+string])
-    plt.show()
-
-
-plot_graphs(history, 'acc')
-plot_graphs(history, 'loss')
-
+model.save('keras_MLP.h5')
